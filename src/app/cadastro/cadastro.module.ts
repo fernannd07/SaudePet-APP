a8d6b3bd3f9f262d7c21fbb04a7e68f30508def1
fernando neto <44534728+fernannd07@users.noreply.github.com>
2022-05-17 12:05:44 -0300

    first commit

---
+++ src/app/cadastro/cadastro.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { CadastroPageRoutingModule } from './cadastro-routing.module';
+
+import { CadastroPage } from './cadastro.page';
+import { InputModule } from '../components/input/input.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    InputModule,
+    CadastroPageRoutingModule
+  ],
+  declarations: [CadastroPage]
+})
+export class CadastroPageModule {}
